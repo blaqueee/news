4371daae567ca0060b5ea8e97139938fb13b5f9d
blaqueee <kajrattobokelov2@gmail.com>
2023-01-14 19:11:31 +0600

    init project

---
+++ src/main/java/edu/megalab/news/MegalabApplication.java
@@ -0,0 +1,13 @@
+package edu.megalab.news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegalabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MegalabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/megalab/news/MegalabApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.megalab.news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegalabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
