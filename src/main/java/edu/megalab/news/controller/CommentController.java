9f08d87caf3bac651ecd8417a3464eb285cbcb69
blaqueee <kajrattobokelov2@gmail.com>
2023-01-15 22:07:12 +0600

    added some endpoints for comment's controller to get comments by postId, parentId

--- src/main/java/edu/megalab/news/controller/CommentController.java
+++ src/main/java/edu/megalab/news/controller/CommentController.java
@@ -5,12 +5,12 @@ import edu.megalab.news.dto.request.CommentRequest;
 import edu.megalab.news.entity.User;
 import edu.megalab.news.service.CommentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -24,5 +24,17 @@ public class CommentController {
     public ResponseEntity<CommentDto> createComment(@Valid @RequestBody CommentRequest commentForm,
                                                     @AuthenticationPrincipal User user) {
         return ResponseEntity.ok(commentService.createComment(commentForm, user));
+    }
+
+    @GetMapping("/posts")
+    public ResponseEntity<Page<CommentDto>> getByPostId(@RequestParam(name = "id") Long postId,
+                                                        @PageableDefault(page = 0, size = 5) Pageable pageable) {
+        return ResponseEntity.ok(commentService.findByPostId(postId, pageable));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<CommentDto>> getByParentId(@RequestParam(name = "id") Long parentId,
+                                                          @PageableDefault(page = 0, size = 5) Pageable pageable) {
+        return ResponseEntity.ok(commentService.findByParentId(parentId, pageable));
     }
 }
--- src/main/java/edu/megalab/news/service/CommentService.java
+++ src/main/java/edu/megalab/news/service/CommentService.java
@@ -3,17 +3,22 @@ package edu.megalab.news.service;
 import edu.megalab.news.dto.CommentDto;
 import edu.megalab.news.dto.request.CommentRequest;
 import edu.megalab.news.entity.Comment;
+import edu.megalab.news.entity.Post;
 import edu.megalab.news.entity.User;
 import edu.megalab.news.exception.NotFoundException;
 import edu.megalab.news.mapper.CommentMapper;
 import edu.megalab.news.repository.CommentRepository;
+import edu.megalab.news.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class CommentService {
     private final CommentRepository commentRepository;
+    private final PostRepository postRepository;
     private final CommentMapper commentMapper;
 
     public CommentDto createComment(CommentRequest form, User user) throws NotFoundException {
@@ -28,5 +33,19 @@ public class CommentService {
             return null;
         return commentRepository.findById(parentId)
             .orElseThrow(() -> new NotFoundException("Comment with id " + parentId + " not found!"));
+    }
+
+    public Page<CommentDto> findByPostId(Long postId, Pageable pageable) throws NotFoundException {
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new NotFoundException("Post with id " + postId + " not found!"));
+        var comments = commentRepository.findByPostAndParentIsNull(post, pageable);
+        return comments.map(commentMapper::toDto);
+    }
+
+    public Page<CommentDto> findByParentId(Long parentId, Pageable pageable) throws NotFoundException {
+        Comment parent = commentRepository.findById(parentId)
+                .orElseThrow(() -> new NotFoundException("Comment with id " + parentId + " not found!"));
+        var comments = commentRepository.findByParent(parent, pageable);
+        return comments.map(commentMapper::toDto);
     }
 }
